7f0c5cb53ea528ee11fe015968e64c24b7a9fb41
wexthang <166706499+wexthang@users.noreply.github.com>
2024-04-11 19:36:52 +0700

    Update lib.rs

--- programs/solquad/src/lib.rs
+++ programs/solquad/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("5sFUqUTjAMJARrEafMX8f4J1LagdUQ9Y8TR8HwGNHkU8");
+declare_id!("3nXb7SfewRBRkcHjtcp9XfLFD4hUPWFvmSSDgStbzfty");
 
 #[program]
 pub mod solquad {
